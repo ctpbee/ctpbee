ba51425324eff946790e5d36c9344fc364caea53
somewheve <somewheve@gmail.com>
2019-08-08 19:48:43 +0800

    fix erroes

--- ctpbee/__init__.py
+++ ctpbee/__init__.py
@@ -1,7 +1,9 @@
-__version__ = '0.23'
+__version__ = '0.24'
 __status__ = 'dev'
 from ctpbee.app import CtpBee
 from ctpbee.context import current_app, switch_app, get_app
-from ctpbee.func import cancle_order, send_order, CtpbeeApi, subscribe, query_func, auth_time, helper
+from ctpbee.func import cancle_order, send_order, CtpbeeApi, subscribe, query_func, auth_time, helper, AsyncApi
 from ctpbee.json import dumps, loads
 from ctpbee.signals import send_monitor, cancle_monitor
+
+
