a934862cb1398253bf8595528e363e94b376665f
somewheve <somewheve@gmail.com>
2019-09-16 16:37:40 +0800

    0.31

--- ctpbee/__init__.py
+++ ctpbee/__init__.py
@@ -2,8 +2,8 @@
     for the future of  life
 """
 
-__version__ = '0.30.3'
-__status__ = '0.30修复版本'
+__version__ = '0.31'
+__status__ = 'beta'
 
 from ctpbee.app import CtpBee
 from ctpbee.context import current_app, switch_app, get_app, del_app
