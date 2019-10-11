f88ece73db1b8cb3e626fe83ca246562f1ec02ca
somewheve <somewheve@gmail.com>
2019-10-11 18:07:16 +0800

    add blueprint demo

---
+++ ctpbee/blueprint.py
@@ -0,0 +1,20 @@
+""" Here are the blueprint module /这里是蓝图模块 """
+
+
+class BluePrint:
+    """ 支持以单账户实现注册 """
+
+    def __init__(self, info, mode, refresh):
+        pass
+
+    def add_extension(self, extension):
+        pass
+
+    def suspend_extension(self, extension_name):
+        pass
+
+    def enable_extension(self, extension_name):
+        pass
+
+    def delete_extension(self, extension_name):
+        pass
