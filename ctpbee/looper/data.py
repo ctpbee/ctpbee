dba8d4e8715a3f7a9b7e8dfac5eceeba67e072e4
somewheve <somewheve@gmail.com>
2019-12-17 16:00:27 +0800

    fix

--- ctpbee/looper/data.py
+++ ctpbee/looper/data.py
@@ -9,6 +9,7 @@ todo: 优化数据访问速度
 """
 
 # todo: 将各家数据转化为从ctpbee数据包 ^_^ alse it will be a good idea to
+from datetime import datetime
 from itertools import chain
 
 
@@ -20,13 +21,36 @@ class Bumblebee(dict):
     __delattr__ = dict.__delitem__
     __getattribute__ = dict.get
 
+    datetime_type = "datetime"
+
     def __init__(self, **kwargs):
         if "last_price" in kwargs:
             self['type'] = "tick"
         else:
             self['type'] = "bar"
         super().__init__(**kwargs)
-        # [setattr(self, key, kwargs.get(key)) for key in kwargs if key in self.__slots__]
+
+        # 需要在此处自动转换datetime数据类型
+        self.datetime = self.covert_datetime(self.datetime)
+
+    def covert_datetime(self, datetime_data):
+        """
+        此函数接受三种格式的数据转换过程
+        :param datetime_data  str/int
+        """
+        if isinstance(datetime_data, datetime):
+            return
+        if isinstance(datetime_data, str):
+            """ 支持.f 或者非.f的构建 """
+            try:
+                return datetime.strptime(date_string=datetime_data, format="%Y-%m-%d %H:%M:%S")
+            except Exception:
+                return datetime.strptime(date_string=datetime_data, format="%Y-%m-%d %H:%M:%S.%f")
+        if isinstance(datetime_data, int):
+            """
+            判断s/us/ns的转换
+            """
+            return datetime.fromtimestamp(datetime_data)
 
 
 class VessData:
--- examples/coding.py
+++ examples/coding.py
@@ -1,3 +1,4 @@
 from flask import Flask
+
 app = Flask(__name__)
 app.run()
