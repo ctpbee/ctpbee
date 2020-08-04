94417a831e41df3a6cf49b3e7270517d0922e957
somewheve <somewheve@gmail.com>
2020-08-04 17:41:53 +0800

    update

--- examples/looper_example.py
+++ examples/looper_example.py
@@ -44,12 +44,12 @@ if __name__ == '__main__':
     data_support = QADataSupport(host="quantaxis.tech", port=27027)
     runnning = Vessel()
     strategy = DoubleMa("ma")
-    data = data_support.get_future_min("rb2010.SHFE", frq="1min", start="2020-07-01", end="2020-07-15")
+    data = data_support.get_future_min("rb2010.SHFE", frq="1min", start="2020-03-01", end="2020-07-15")
     runnning.add_data(data)
     params = {
         "looper":
             {
-                "initial_capital": 100000,
+                "initial_capital": 50000,
                 "deal_pattern": "price",
                 # 合约乘数
                 "size_map": {"rb2010.SHFE": 10},
