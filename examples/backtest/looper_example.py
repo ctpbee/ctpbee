ca01d4b47fd702684d3ee416934703bcd87fca43
somewheve <somewheve@gmail.com>
2024-01-17 22:35:54 +0800

    [update] examples

--- examples/backtest/looper_example.py
+++ examples/backtest/looper_example.py
@@ -6,47 +6,9 @@ data = pd.read_csv("kline.csv")
 data = data.drop("Unnamed: 0", axis=1)
 data = [list(reversed(data.to_dict("index").values()))]
 
-
-class DoubleMaStrategy(CtpbeeApi):
-    def __init__(self, name):
-        super().__init__(name)
-        self.instrument_set = ["rb2401.SHFE"]
-        self.fast_window = 10
-        self.slow_window = 20
-        self.pos = 0
-        self.open = False
-        self.price = []
-        self.open = False
-        self.open_price = None
-        self.buy = 0
-        self.sell = 0
-        self.slow = 60
-        self.fast = 30
-        self.buy = False
-        self.count = 0
-
-    def on_trade(self, trade: TradeData):
-        pass
-
-    def on_order(self, order: OrderData) -> None:
-        pass
-
-    def on_bar(self, bar):
-        """ """
-        self.count += 1
-        if self.count % 5 == 0:
-            self.action.buy_open(bar.close_price, 1, bar)
-
-    def on_tick(self, tick):
-        pass
-
-    def on_init(self, init: bool):
-        print("初始化成功了")
-
+from examples.strategy.double_ma import DoubleMa
 
 if __name__ == '__main__':
-    from ctpbee import CtpbeeApi
-
     code = "rb2401.SHFE"
     app = CtpBee("looper", __name__)
     info = {
@@ -58,7 +20,8 @@ if __name__ == '__main__':
             },
             "commission_ratio": {
                 code: {
-                    "close": 0.00001
+                    "close": 0.00001,
+                    "close_today": 0.00001,
                 },
             },
             "size_map": {
@@ -69,7 +32,7 @@ if __name__ == '__main__':
     app.config.from_mapping(info)
     app.add_local_contract(ContractData(local_symbol=code, exchange=Exchange.SHFE, symbol=code.split(".")[0],
                                         size=10, pricetick=1))
-    strategy = DoubleMaStrategy("ma")
+    strategy = DoubleMa("ma", code)
     app.add_extension(strategy)
     app.add_data(*data)
     app.start()
