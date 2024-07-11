96e94774e6148def1ee384b8c69eb52cb7699c2f
somewheve <somewheve@gmail.com>
2024-07-11 21:50:19 +0800

    change account

--- examples/login.py
+++ examples/login.py
@@ -5,12 +5,13 @@ from ctpbee.constant import *
 
 
 class Main(CtpbeeApi):
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, code="ag2406"):
+        super().__init__(code)
         self.init = False
         self.ok = 0
         self.pos = None
         self.pos_init = False
+        self.code = code
 
     def on_tick(self, tick: TickData) -> None:
         if self.init and self.ok == 0:
@@ -20,23 +21,20 @@ class Main(CtpbeeApi):
     def on_trade(self, trade: TradeData) -> None:
         print("成交回报", trade)
 
+        pos = self.center.get_position(f"{self.code}.SHFE")
+        print(pos.long_volume, pos.short_volume)
+
     def on_order(self, order: OrderData) -> None:
         if self.init:
             print("订单回报: ", order)
         print(order)
 
     def on_position(self, position: PositionData) -> None:
-        # print("持仓回报", position)
-        pos = self.center.get_position("ag2406.SHFE")
+        pos = self.center.get_position(f"{self.code}.SHFE")
         print(pos.long_volume, pos.short_volume)
-        # if pos != self.pos:
-        #     print("===>", pos)
-        #     self.pos = pos
-        #     if not self.pos_init:
-        #         self.pos_init = True
 
     def on_contract(self, contract: ContractData):
-        if contract.symbol == "ag2406":
+        if contract.symbol == self.code:
             self.action.subscribe(contract.local_symbol)
 
     def on_init(self, init: bool):
@@ -45,7 +43,7 @@ class Main(CtpbeeApi):
 
 if __name__ == '__main__':
     app = CtpBee("market", __name__, refresh=True)
-    example = Main("DailyCTA")
+    example = Main("rb2410")
     app.config.from_json("config.json")
     app.add_extension(example)
     app.start(log_output=True)
