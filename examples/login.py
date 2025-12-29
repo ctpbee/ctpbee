cf8e229b77c01e833ccc17fed546d4164a4ad132
somewheve <somewheve@gmail.com>
2025-12-29 18:09:56 +0800

    rev

--- examples/login.py
+++ examples/login.py
@@ -3,13 +3,13 @@
 请进行修改
 """
 
-from ctpbee import CtpBee
-from ctpbee.constant import *
 from ctpbee_kline import Kline
-from strategy import ATRStrategy
+
+from ctpbee import CtpBee, CtpbeeApi
+from ctpbee.constant import *
 
 """
-此处替换继承的策略类 
+此处替换继承的策略类
 ATRStrategy
 BollingerStrategy
 DoubleMa
@@ -18,41 +18,37 @@ RSIStrategy
 """
 
 
-class CTA(ATRStrategy):
+class CTA(CtpbeeApi):
+    def __init__(
+        self,
+        name,
+    ) -> None:
+        super().__init__(name)
+
     def on_contract(self, contract: ContractData) -> None:
         if len(contract.symbol) > 6:
             return
-        if contract.symbol in self.instrument_set:
-            self.action.subscribe(contract.local_symbol)
+        self.action.subscribe(contract.local_symbol)
+        print("subscribe: ", contract.local_symbol)
 
     def on_tick(self, tick: TickData) -> None:
         """
         可以在此处做出tick级别的止损
         """
-        # print(tick)
+        print(tick)
 
     def on_init(self, init: bool):
         # 此API被回调完成后告知策略执行成功
         self.init = True
         self.info("init success")
 
-    def on_realtime(self):
-        if self.init:
-            print(self.center.active_orders)
-
 
 if __name__ == "__main__":
     kline = Kline()
     app = CtpBee("market", __name__, refresh=True).with_tools(kline)
-    cta = CTA("ag2602", "ag2602")
+    cta = CTA("all")
     # app.config.from_json("config.json")
     # 使用simnow 24小时
-    app.config.from_json("config.json")
+    app.config.from_json("config_24.json")
     app.add_extension(cta)
     app.start(log_output=True)
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        print("\n策略已停止")
-        app.release()
